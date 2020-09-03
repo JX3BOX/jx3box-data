bf18957e27172d4c0ad2728d5ba6852d64a8f350
iRuxu <rx6@qq.com>
2020-09-04 00:50:44 +0800

    update map

--- tools/maplist.js
+++ tools/maplist.js
@@ -14,15 +14,20 @@ fs.writeFileSync('./data/common/maplist.json',JSON.stringify(records))
 
 // console.log(records)
 let idmap = []
+let mapindex = {}
 records.forEach((item) => {
     idmap.push({
         DisplayName : item.DisplayName,
         ID : ~~item.ID
     })
+    mapindex[item.ID] = item.DisplayName
 })
 
 // console.log(idmap)
 fs.writeFileSync('./data/common/maplistids.json',JSON.stringify(idmap))
+fs.writeFileSync('./data/common/mapindex.json',JSON.stringify(mapindex))
+
+
 
 
 
