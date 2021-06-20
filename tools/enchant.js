8978b8d75843b16cd4e0f1701f96b475653824dc
iRuxu <rx6@qq.com>
2021-06-20 15:01:18 +0800

    add enchant data

--- tools/enchant.js
+++ tools/enchant.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const iconv = require("iconv-lite");
 const parse = require('csv-parse/lib/sync')
 
+// 大附魔描述
 let data = fs.readFileSync('./raw/bps/commonenchant.tab')
 data = iconv.decode(Buffer.from(data), "gbk");
 
@@ -14,10 +15,29 @@ let records = parse(data, {
 
 let output = {}
 records.forEach((item) => {
-    item.desc = item.desc.replace('<text>text="','')
-    item.desc = item.desc.replace('" font=12 </text>','')
+    if(item.desc){
+        item.desc = item.desc.replace('<text>text="','')
+        item.desc = item.desc.replace('" font=12 </text>','')
+    }
     output[item.enchant_id] = item.desc
 })
 // console.log(records)
-
 fs.writeFileSync('./data/bps/enchant.json',JSON.stringify(output))
+
+// 附魔属性
+let _data = fs.readFileSync('./raw/bps/Enchant.tab')
+_data = iconv.decode(Buffer.from(_data), "gbk");
+
+let _records = parse(_data, {
+    delimiter: "\t",
+    columns: true,
+    skip_empty_lines: true,
+    quote: null
+})
+
+let _output = {}
+_records.forEach((item) => {
+    _output[item.ID] = item
+})
+
+fs.writeFileSync('./data/bps/_enchant.json',JSON.stringify(_output))
