0d81dde046e6716de0cde863b1db69157a845a9c
whc2001 <ianwang0122@outlook.com>
2022-07-18 04:09:22 -0400

    改为技能ID和名称分别输出图标数据

--- skill_icon.js
+++ skill_icon.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
+const path = require("path");
 const csvparse = require("csv-parse");
 const iconv = require("iconv-lite");
 
 function Main(args) {
     let skillIDLevelAndIcon = {};
+    let skillNameAndIcon = {};
 
     const fileStream = fs.createReadStream(args[0]);
     const encodingConverter = iconv.decodeStream("gbk");
@@ -15,16 +17,19 @@ function Main(args) {
 
     parser.on("readable", () => {
         while ((line = parser.read()) !== null) {
+            const skillName = line["Name"];
             const skillID = ~~line["SkillID"];
             const skillLevel = ~~line["Level"];
             const skillIcon = ~~line["IconID"];
             if(!skillIDLevelAndIcon.hasOwnProperty(skillID))
                 skillIDLevelAndIcon[skillID] = {};
             skillIDLevelAndIcon[skillID][skillLevel] = skillIcon;
+            skillNameAndIcon[skillName] = skillIcon;
         }
     });
     parser.on("end", () => {
-        fs.writeFileSync(args[1], JSON.stringify(skillIDLevelAndIcon));
+        fs.writeFileSync(path.join(args[1], "skill_id_icon.json"), JSON.stringify(skillIDLevelAndIcon));
+        fs.writeFileSync(path.join(args[1], "skill_name_icon.json"), JSON.stringify(skillNameAndIcon));
     });
 
     fileStream.pipe(encodingConverter).pipe(parser);
